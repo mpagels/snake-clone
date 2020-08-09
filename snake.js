17479eb53a142fae52bf9e495acb6eb35a9770ba
Martin <office@martinpagels.de>
2020-08-10 01:01:03 +0200

    Add prey and bodygrow after eat

--- snake.js
+++ snake.js
@@ -8,52 +8,68 @@ const ctx = canvas.getContext('2d')
 let moveX = 5
 let moveY = 0
 
-const snakeHead = {
-  x: 20,
-  y: 20,
-  sizeX: 5,
-  sizeY: 5,
-}
-
-const snakeBody = [
+const snake = [
+  {
+    x: 20,
+    y: 20,
+    sizeX: 5,
+    sizeY: 5,
+    currentMove: 'ArrowRight',
+  },
   {
     x: 15,
     y: 20,
     sizeX: 5,
     sizeY: 5,
+    currentMove: 'ArrowRight',
+    nextMove: 'ArrowRight',
   },
   {
     x: 10,
     y: 20,
     sizeX: 5,
     sizeY: 5,
+    currentMove: 'ArrowRight',
+    nextMove: 'ArrowRight',
   },
 ]
 
 let moveCommand = 'ArrowRight'
-let lastMoveCommand = 'ArrowRight'
-let currentMoveList = ['ArrowRight']
-let nextMoveList = []
+
+let prey = getPrey()
 
 main()
 
 function main() {
   ctx.clearRect(0, 0, 600, 600)
-  let move = getMoves(moveCommand)
-  snakeHead.x += move.moveX
-  snakeHead.y += move.moveY
-  currentMoveList.push(moveCommand)
+  ctx.fillRect(prey.x, prey.y, prey.sizeX, prey.sizeY)
+  //ctx.fillRect(snakeHead.x, snakeHead.y, snakeHead.sizeX, snakeHead.sizeY)
+  snake.forEach((snakePart, i, snake) => {
+    if (i === 0) {
+      let move = getMoves(moveCommand)
+      snakePart.x += move.moveX
+      snakePart.y += move.moveY
+      snake[i + 1].nextMove = moveCommand
 
-  ctx.fillRect(snakeHead.x, snakeHead.y, snakeHead.sizeX, snakeHead.sizeY)
-  snakeBody.forEach((snakePart, i) => {
-    const move = getMoves(currentMoveList[i + 1])
-    snakePart.x += move.moveX
-    snakePart.y += move.moveY
-    currentMoveList.push(currentMoveList[i + 1])
-
+      // prey is eaten logic
+      if (
+        snakePart.x >= prey.x &&
+        snakePart.x <= prey.x + prey.sizeX &&
+        snakePart.y >= prey.y &&
+        snakePart.y <= prey.y + prey.sizeY
+      ) {
+        prey = getPrey()
+        growSnake()
+      }
+    } else {
+      let move = getMoves(snakePart.currentMove)
+      snakePart.x += move.moveX
+      snakePart.y += move.moveY
+      snakePart.currentMove = snakePart.nextMove
+      snakePart.nextMove = snake[i - 1].currentMove
+    }
     ctx.fillRect(snakePart.x, snakePart.y, snakePart.sizeX, snakePart.sizeY)
   })
-  currentMoveList = [moveCommand]
   requestAnimationFrame(main)
 }
 
@@ -91,3 +107,64 @@ function getMoves(moveCommand) {
 window.addEventListener('keydown', (event) => {
   moveCommand = event.key
 })
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min)
+}
+
+function getPrey() {
+  return {
+    sizeX: 20,
+    sizeY: 20,
+    x: getRandomNumber(5, 595),
+    y: getRandomNumber(5, 595),
+    isEaten: false,
+  }
+}
+
+function growSnake() {
+  snake.forEach((snake) => console.log(snake))
+  const lastBodyPart = snake[snake.length - 1]
+  const push = getNewBody(lastBodyPart.currentMove)
+  snake.push({
+    x: lastBodyPart.x,
+    y: lastBodyPart.y,
+    sizeX: 5,
+    sizeY: 5,
+    currentMove: lastBodyPart.currentMove,
+    nextMove: lastBodyPart.nextMove,
+  })
+  console.log('lastbodypart:', lastBodyPart)
+  console.log('new:', snake[snake.length - 1])
+}
+
+function getNewBody(moveCommand) {
+  const move = {
+    ArrowDown: function () {
+      return {
+        moveY: -5,
+        moveX: 0,
+      }
+    },
+    ArrowUp: function () {
+      return {
+        moveY: 5,
+        moveX: 0,
+      }
+    },
+    ArrowRight: function () {
+      return {
+        moveY: 0,
+        moveX: -5,
+      }
+    },
+    ArrowLeft: function () {
+      return {
+        moveY: 0,
+        moveX: 5,
+      }
+    },
+  }
+  const erg = move[moveCommand]
+  return erg()
+}
