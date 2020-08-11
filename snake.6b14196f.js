2a41dfab2d177a0ba0de9393659a05ace45cf220
Martin <office@martinpagels.de>
2020-08-11 10:29:26 +0200

    prepare for gh-pages

--- snake.6b14196f.js
+++ snake.6b14196f.js
@@ -352,7 +352,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49681" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49741" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -529,4 +529,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js","game/snake.js"], null)
-//# sourceMappingURL=/snake.6b14196f.js.map
+//# sourceMappingURL=snake.6b14196f.js.map
